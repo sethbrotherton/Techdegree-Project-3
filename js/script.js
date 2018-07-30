db282214558bfd15fdf0335f92d23f6cc6467bd2
sethbrotherton <sethbrotherton@yahoo.com>
2018-07-30 02:45:29 -0400

    dynamic registration for activities added

--- js/script.js
+++ js/script.js
@@ -39,3 +39,80 @@ shirtDesign.change(function(){
       shirtColor.show();
     }
   });
+
+  const activities = $('.activities');
+  const checkboxes = $('[type="checkbox"]');
+  const mainConference = $('.activities [name="all"]');
+  const jsFrameworks = $('.activities [name="js-frameworks"]');
+  const jsLibraries = $('.activities [name="js-libs"]');
+  const express = $('.activities [name="express"]');
+  const node = $('.activities [name="node"]');
+
+
+
+
+activities.change(function(e) {
+  $('.total').remove();
+  let total = 0;
+  let totalMessage = '';
+  if (checkboxes[0].checked == true) {
+    total += 200;
+  }
+  for (let i = 1; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked == true) {
+      total += 100;
+    }
+  }
+
+
+   for (let i = 0; i < checkboxes.length; i++) {
+     if (checkboxes[1].checked == true) {
+       express.attr("disabled", true);
+     } else {
+       express.attr("disabled", false);
+     }
+     if (checkboxes[3].checked == true) {
+       jsFrameworks.attr("disabled", true);
+     } else {
+       jsFrameworks.attr("disabled", false);
+     }
+     if (checkboxes[2].checked == true) {
+       node.attr("disabled", true);
+     } else {
+       node.attr("disabled", false);
+     }
+     if (checkboxes[4].checked == true) {
+       jsLibraries.attr("disabled", true);
+     } else {
+       jsLibraries.attr("disabled", false);
+     }
+
+   }
+ //let total = 0;
+  totalMessage += `<span class='total'>Total: $${total}</span>`;
+  if (total > 0) {
+    activities.append(totalMessage);
+  }
+});
+
+
+// if (e.target.checked == true && e.target.name == 'js-frameworks') {
+//   express.attr("disabled", true);
+// } else {
+//   express.attr("disabled", false);
+// }
+//   if (e.target.checked == true && e.target.name == 'express') {
+//     jsFrameworks.attr("disabled", true);
+//   } else {
+//     jsFrameworks.attr("disabled", false);
+//   }
+// if (e.target.checked == true && e.target.name == 'js-libs') {
+//   node.attr("disabled", true);
+// } else {
+//   node.attr("disabled", false);
+// }
+// if (e.target.checked == true && e.target.name == 'node') {
+//   jsLibraries.attr("disabled", true);
+// } else {
+//   jsLibraries.attr("disabled", false);
+// }
